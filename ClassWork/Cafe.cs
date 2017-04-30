dc5e66dd4a8da140084071aa9dd8a7fe3914681c
Vanush Voskanyan <vanush.voskanyan@mail.ru>
2017-04-30 12:04:46 +0400

    Updates in Cafe.cs

--- ClassWork/Cafe.cs
+++ ClassWork/Cafe.cs
@@ -1,53 +1,138 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Device.Location;
 
-namespace ClassWork
+namespace Classwork
 {
-    class Cafe:IComparable
+    class Cafe 
     {
-        public string Name { get; private set; }
-        public string phoneNumber { get; private set; }
-        public GeoCoordinate Adress
+        //
+        // List of objects(cafes)(Initialization @see CafeProject.Source.cs):        
+
+        public static List<Cafe> ListOfCafes = new List<Cafe>();
+
+        //
+        // Initialization of cafes menu:
+        public Menu Menu = new Menu();
+        //
+        // Fields:
+        public static int count = ListOfCafes.Count;
+
+
+        private string name;
+        private string rank;
+        private string phoneNumber;
+        public string adress;
+        public string webPage;
+        public string email;
+        // 
+        // Constructors:
+
+        public Cafe(string name, string rank, string phoneNumber, string adress, string webPage, string email)
+        {
+            this.Name = name;
+            this.PhoneNumber = phoneNumber;
+            this.Adress = adress;
+            if (rank.Equals("")) { this.Rank = "not specefied"; }
+            else { this.Rank = rank; }
+            if (webPage.Equals("")) { this.WebPage = "not specefied"; }
+            else { this.WebPage = webPage; }
+            if (email.Equals("")) { this.Email = "not specefied"; }
+            else { this.Email = email; }
+        }
+
+        //
+        // Properties:
+        public string Name
         {
             get
             {
-                return Adress;
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+        public string Rank
+        {
+            get
+            {
+                return this.rank;
+            }
+            private set
+            {
+                if (double.Parse(value) > 10 || double.Parse(value) < 0)
+                {
+                    Console.WriteLine("Please enter value in range 0-10");
+                }
+                else
+                {
+                    this.rank = value;
+                }
+            }
+        }
+        public string PhoneNumber
+        {
+            get
+            {
+                return this.phoneNumber;
             }
             set
             {
-                Adress = value;
+                this.phoneNumber = value;
             }
         }
-        public double Rating { get; private set; }
-        public OfficialPage officialPage = new OfficialPage();
-        public Cafe(string name, string phoneNumber, GeoCoordinate adress, double rating)
+        public string Adress
         {
-            this.Name = name;
-            this.Adress = adress;
-            this.phoneNumber = phoneNumber;
-            this.Rating = rating;
-            
+            get
+            {
+                return this.adress;
+            }
+            set
+            {
+                this.adress = value;
+            }
         }
+        public string WebPage
+        {
+            get
+            {
+                return this.webPage;
+            }
+            set
+            {
+                this.webPage = value;
+            }
 
-        public int CompareTo(Object obj)
+        }
+        public string Email
         {
-            Cafe cafe = (Cafe)obj;
-            if (this.rating > cafe.rating)
+            get
             {
-                return -1;
+                return this.email;
             }
-            else if (this.rating < cafe.rating)
+            set
             {
-                return 1;
-            }
-            else
-            {
-                return 0;
+                this.email = value;
             }
         }
-    }    
+        //
+        // Methods
+    
+
+        //
+        // Getting information about object(cafe):
+        public override string ToString()
+        {
+            string information;
+            information = "___________________________________________\n";
+            information += String.Format("Cafes name : {0} \nRank : {1} \nPhone Number : {2} \nAdress : {3} \nWeb Page URL : {4} \nEmail : {5} \n"
+                , this.Name, this.Rank, this.PhoneNumber, this.Adress, this.WebPage, this.Email);
+            information += "___________________________________________";
+            return information;
+        }       
+    }
 }
